b2b0a7263e61e1ce7a48efbf374922f81ad446b9
Tshimangadzo <mudzanani27@gmail.com>
2022-08-30 11:39:08 +0200

    added use db

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- create a db and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id int NOT Null   AUTO_INCREMENT  ,
     name VARCHAR(256) Not NULL,
