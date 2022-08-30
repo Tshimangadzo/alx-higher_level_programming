41eeffeb84d35724f102195c79a4b5d7292779e6
Tshimangadzo <mudzanani27@gmail.com>
2022-08-30 11:35:37 +0200

    added states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,8 @@
+-- create a db and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    Id int NOT Null UNIQUE  AUTO INCREMENT  PRIMARY KEY,
-    name VARCHAR(256) Not NULL
+    id int NOT Null   AUTO_INCREMENT  ,
+    name VARCHAR(256) Not NULL,
+    PRIMARY KEY (id),
+    UNIQUE(id)
 );
