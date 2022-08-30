aedf257e2db2731411bb7005377d476df80e36d6
Tshimangadzo <mudzanani27@gmail.com>
2022-08-30 13:17:17 +0200

    added cities

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+--CREATE DATABASE
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT AUTO_INCREMENT NOT NULL,
+    state_id int NOT NULL,
+    name VARCHAR (256) NOT NULL,
+    UNIQUE (id),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES states(id),
+)
+
