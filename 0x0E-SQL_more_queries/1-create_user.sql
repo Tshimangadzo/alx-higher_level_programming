ed5b878fb2cdeb505f2294944ec55ed58b7e6fb6
Tshimangadzo <mudzanani27@gmail.com>
2022-08-30 09:30:43 +0200

    added a fix to grant

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 --/ Create a user and grant all permisions
-
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON `database`.* TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
