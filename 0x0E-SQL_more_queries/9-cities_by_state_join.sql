e09d5e4a08f2dcfb0d909f9e499a332fb3132e88
Tshimangadzo <mudzanani27@gmail.com>
2022-08-31 09:14:42 +0200

    added left join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 --  lists all cities contained in the database
 
 SELECT c.id, c.name, s.name
-from cities as c JOIN states as s on c.state_id = s.id;
+from cities as c LEFT JOIN states as s on c.state_id = s.id;
