bfa8909f259483930dd18aab5f6e630c4b854619
Tshimangadzo <mudzanani27@gmail.com>
2022-08-31 12:02:10 +0200

    added ordered by

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,4 +3,4 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 from tv_shows, tv_show_genres
 WHERE tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
