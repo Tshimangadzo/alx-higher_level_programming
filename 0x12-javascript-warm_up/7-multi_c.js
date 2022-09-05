2f2bf45542f4292f7a3b06e01c20277e96f78016
Tshimangadzo Mudznani <mudzanani27@gmail.com>
2022-09-05 13:28:48 +0200

    added multi c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const argNumber = parseInt(process.argv[2], 10);
+if (!argNumber) {
+  console.log("Missing number of occurrences");
+}
+for (let i = 0; i < argNumber; i++) {
+  console.log("C is fun");
+}
