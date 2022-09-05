be736507fa332a01b0349b71d770fcb33c85976e
Tshimangadzo Mudznani <mudzanani27@gmail.com>
2022-09-05 13:41:07 +0200

    added 8-square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const argsNumber = parseInt(process.argv[2], 10);
+
+if (!argsNumber) {
+  console.log("Missing size");
+}
+
+for (let row = 0; row < argsNumber; row++) {
+  let rowsData = "";
+  for (let cols = 0; cols < argsNumber; cols++) {
+    rowsData += "X";
+  }
+  console.log(rowsData);
+}
