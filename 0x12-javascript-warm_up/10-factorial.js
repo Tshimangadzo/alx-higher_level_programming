aa5f6587774422c0628a990989b753a5ef7ff143
Tshimangadzo Mudznani <mudzanani27@gmail.com>
2022-09-05 14:30:48 +0200

    added factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const argsNumber = parseInt(process.argv[2], 10);
+
+function factorial(number) {
+  if (number === 0) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
+}
+
+if (!argsNumber) {
+  console.log(1);
+}else{
+    console.log(factorial(argsNumber))
+}
